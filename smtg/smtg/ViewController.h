f3456df41bc3d45519cc4309349ba9240a82c72b
wangbaolin <wangbaolin@wangbaolindemacbook-pro.local>
2016-03-16 15:56:56 +0800

    初始化项目

---
+++ smtg/smtg/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  smtg
+//
+//  Created by 王保霖 on 16/3/16.
+//  Copyright © 2016年 Ricky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
